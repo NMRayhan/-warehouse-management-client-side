bf30d7055d95b300d3fdf9cae6204ccdc7629209
NMRayhan <nurmdrayhan97@gmail.com>
2022-05-21 20:59:30 +0600

    header component complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./component/Home/Header/Header";
+import Footer from "./component/Home/Footer/Footer";
+import Blog from "./component/Common/Blog/Blog";
+import AddProduct from "./component/AddProduct/AddProduct";
+import Products from "./component/ShowingProduct/Products/Products";
+import MyProduct from "./component/ShowingProduct/MyProduct/MyProduct";
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
+      <Routes>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/addProduct" element={<AddProduct></AddProduct>}></Route>
+        <Route path="/manageInventory" element={<Products></Products>}></Route>
+        <Route path="/myAdded" element={<MyProduct></MyProduct>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/component/Home/Header/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import CustomLink from "../../Common/CustomLink/CustomLink";
+import "../Header/Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link className="custom-nav-item">
+                <CustomLink to="/">Home</CustomLink>
+              </Nav.Link>
+              <Nav.Link className="custom-nav-item">
+                <CustomLink to="/blog">Blog</CustomLink>
+              </Nav.Link>
+              <Nav.Link className="custom-nav-item">
+                <CustomLink to="/addProduct">Add Product</CustomLink>
+              </Nav.Link>
+              <Nav.Link className="custom-nav-item">
+                <CustomLink to="/manageInventory">Manage Inventory</CustomLink>
+              </Nav.Link>
+              <Nav.Link className="custom-nav-item">
+                <CustomLink to="/myAdded">My added Product</CustomLink>
+              </Nav.Link>
+            </Nav>
+            <Nav className="ms-auto">
+              <Nav.Link href="#">Nur Mohammad Rayhan</Nav.Link>
+              <Button variant="secondary">Sign Out</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
