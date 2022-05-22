0e1a8ee0cda2316d87896db49295b7a510a0e519
NMRayhan <nurmdrayhan97@gmail.com>
2022-05-22 10:43:31 +0600

    Product update successfully done

--- src/component/ShowingProduct/UpdateProduct/UpdateProduct.js
+++ src/component/ShowingProduct/UpdateProduct/UpdateProduct.js
@@ -1,28 +1,86 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Col, Container, Form, Row } from "react-bootstrap";
-import { useForm } from "react-hook-form";
-import { useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 
 const UpdateProduct = () => {
   const [details, setDetails] = useState([]);
-  const {_id} = useParams();
+  const { _id } = useParams();
+  const navigate = useNavigate();
+
   useEffect(() => {
     const url = `http://localhost:5000/product/details/${_id}`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-          setDetails(data)
-          console.log(data)
+        setDetails(data);
       });
   }, [_id]);
 
+  const handleNameChange = (e) => {
+    const { Product_Name, ...rest } = details;
+    const newName = e.target.value;
+    const newDetails = { Product_Name: newName, ...rest };
+    setDetails(newDetails);
+  };
+  const handleImgURLChange = (e) => {
+    const { Product_Image_URL, ...rest } = details;
+    const newName = e.target.value;
+    const newDetails = { Product_Image_URL: newName, ...rest };
+    setDetails(newDetails);
+  };
 
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    const url = "http://localhost:5000/addProduct";
+  const handleDescriptionChange = (e) => {
+    const { Short_Description, ...rest } = details;
+    const newName = e.target.value;
+    const newDetails = { Short_Description: newName, ...rest };
+    setDetails(newDetails);
+  };
+
+  const handlePriceChange = (e) => {
+    const { Price, ...rest } = details;
+    const newName = e.target.value;
+    const newDetails = { Price: newName, ...rest };
+    setDetails(newDetails);
+  };
+
+  const handleQuantityChange = (e) => {
+    const { Quantity, ...rest } = details;
+    const newName = e.target.value;
+    const newDetails = { Quantity: newName, ...rest };
+    setDetails(newDetails);
+  };
+
+  const handleSupplierChange = (e) => {
+    const { Supplier_Name, ...rest } = details;
+    const newName = e.target.value;
+    const newDetails = { Supplier_Name: newName, ...rest };
+    setDetails(newDetails);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const Admin_email = event.target.Admin_email.value;
+    const Product_Name = event.target.Product_Name.value;
+    const Supplier_Name = event.target.Supplier_Name.value;
+    const Quantity = event.target.Quantity.value;
+    const Price = event.target.Price.value;
+    const Short_Description = event.target.Short_Description.value;
+    const Product_Image_URL = event.target.Product_Image_URL.value;
+
+    const data = {
+      Product_Name: Product_Name,
+      Product_Image_URL: Product_Image_URL,
+      Short_Description: Short_Description,
+      Price: Price,
+      Quantity: Quantity,
+      Supplier_Name: Supplier_Name,
+      Admin_email: Admin_email
+    };
+
+    const url = `http://localhost:5000/product/update/${_id}`;
     fetch(url, {
-      method: "POST",
+      method: "PUT",
       headers: {
         "content-type": "application/json",
       },
@@ -30,63 +88,84 @@ const UpdateProduct = () => {
     })
       .then((request) => request.json())
       .then((data) => {
-        toast("Product Added Successfully");
+        console.log(data);
+        toast("Product Update Successfully");
+        setInterval(() => {
+            navigate("/manageInventory");
+        }, 3000);
       });
   };
-
 
   return (
     <div>
       <Container>
-      <h2 className="text-center text-success fw-bold mt-3">Update Product </h2>
-      <ToastContainer />
-        <Form onSubmit={handleSubmit(onSubmit)}>
+        <h2 className="text-center text-success fw-bold mt-3">
+          Update Product{" "}
+        </h2>
+        <ToastContainer />
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mt-3">
             <Form.Label>Admin Email</Form.Label>
             <Form.Control
               type="email"
-              {...register("Admin_email", { required: true })}
+              value={details.Admin_email}
+              readOnly
+              disabled
+              name="Admin_email"
             />
           </Form.Group>
           <Form.Group className="mt-3">
             <Form.Label>Product Name</Form.Label>
             <Form.Control
               type="text"
-              {...register("Product_Name", { required: true, maxLength: 500 })}
+              value={details.Product_Name}
+              name="Product_Name"
+              onChange={handleNameChange}
             />
           </Form.Group>
           <Form.Group className="mt-3">
             <Form.Label>Product Image URL</Form.Label>
             <Form.Control
               type="text"
-              {...register("Product_Image_URL", { required: true })}
+              value={details.Product_Image_URL}
+              name="Product_Image_URL"
+              onChange={handleImgURLChange}
             />
           </Form.Group>
           <Form.Group className="mt-3">
             <Form.Label>Short Description</Form.Label>
             <Form.Control
-              {...register("Short_Description", { required: true })}
+              type="text"
+              value={details.Short_Description}
+              name="Short_Description"
+              onChange={handleDescriptionChange}
             />
           </Form.Group>
           <Form.Group className="mt-3">
             <Form.Label>Product Price</Form.Label>
             <Form.Control
               type="number"
-              {...register("Price", { required: true })}
+              value={details.Price}
+              name="Price"
+              onChange={handlePriceChange}
             />
           </Form.Group>
           <Form.Group className="mt-3">
             <Form.Label>Product Quantity</Form.Label>
             <Form.Control
               type="number"
-              {...register("Quantity", { required: true, maxLength: 12 })}
+              value={details.Quantity}
+              name="Quantity"
+              onChange={handleQuantityChange}
             />
           </Form.Group>
           <Form.Group className="mt-3">
             <Form.Label>Supplier Name</Form.Label>
             <Form.Control
               type="text"
-              {...register("Supplier_Name", { required: true })}
+              value={details.Supplier_Name}
+              name="Supplier_Name"
+              onChange={handleSupplierChange}
             />
           </Form.Group>
           <Form.Group>
