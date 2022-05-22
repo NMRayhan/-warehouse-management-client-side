bd14159e1c4110fe65fe276c81565a270acda545
NMRayhan <nurmdrayhan97@gmail.com>
2022-05-22 16:09:01 +0600

    firebase

---
+++ src/firebase.init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACWPxmvldQ9bZO0KOeaSI2d97N2WipjfU",
+  authDomain: "bycycle-warehouse.firebaseapp.com",
+  projectId: "bycycle-warehouse",
+  storageBucket: "bycycle-warehouse.appspot.com",
+  messagingSenderId: "313164720133",
+  appId: "1:313164720133:web:a16d03f0793d593f3b946c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
